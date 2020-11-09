e01098f7084ebbcca9c91cf54ebac39b2c80fe6d
Chetan2028 <bmchetan2028@gmail.com>
2020-11-09 15:42:46 +0530

    Add project files.

---
+++ BinarySearchTreeDemo/BinarySearchTree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTreeDemo
+{
+    class BinarySearchTree
+    {
+        Node root;
+
+        public BinarySearchTree()
+        {
+            this.root = null;
+        }
+
+        public void Insert(Node root , int key)
+        {
+            //Create a new node containing a new element
+            Node newNode = new Node(key);
+
+            //Pointer to start traversing from root and traverses downward path to search where the new node to  be inserted
+            Node x = root;
+
+            Node y = null;
+
+            while (x != null)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ BinarySearchTreeDemo/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTreeDemo
+{
+    class Node
+    {
+        public int key;
+        public Node left;
+        public Node right;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Node"/> class.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        public Node(int key)
+        {
+            this.key = key;
+            this.left = null;
+            this.right = null;
+        }
+    }
+}
